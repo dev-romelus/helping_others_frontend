556ed6cdedc4f1e6b5f5a2bb0b554158eff004fe
dev-romelus <dev.romelus@gmail.com>
2022-11-14 22:20:02 +0100

    refacto

--- src/components/helpers/Testimony.js
+++ src/components/helpers/Testimony.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { Avatar } from 'antd';
 
-
 const UnstyledTestimony = ({ className, photo, name, description }) => {
   return (
     <div className={className}>
